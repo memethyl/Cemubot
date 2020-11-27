a4f724644d38a2e4b29ca9366af6574f20223b76
Crementif <26669564+Crementif@users.noreply.github.com>
2020-11-28 00:58:07 +0100

    Fix status
    Previously it would never "release" the patreon release, and it would also not check if there's a patreon release on bootup.

--- cemubot/cogs/site.py
+++ cemubot/cogs/site.py
@@ -17,6 +17,7 @@ class Site(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.updateVersions.start()
+        self.updatePatreonStatus.start()
     
     @commands.command(name="download", help="Search for the game's compatibility wiki page.", aliases=["dl"])
     async def downloadLink(self, ctx, *, version_hint: str):
@@ -40,16 +41,17 @@ class Site(commands.Cog):
                     await ctx.send(content="Are you from the future, or does this version just not exist yet!")
     
     async def update_activity(self):
-        if datetime.utcnow() < self.public_release_date:
-            if self.version_list[0] == self.patreon_release:
-                new_activity = f"Cemu {self.patreon_release} is publicly released!"
-            else:
-                new_activity = f"Public release {self.release_dates[(self.public_release_date-datetime.utcnow()).days+1]}"
+        if len(self.version_list) == 0:
+            return
+
+        print(self.version_list[0], self.patreon_release, datetime.utcnow() < self.public_release_date)
+        if self.version_list[0] is not self.patreon_release and datetime.utcnow() < self.public_release_date:
+            new_activity = f"Public release {self.release_dates[(self.public_release_date-datetime.utcnow()).days+1]}"
         else:
             new_activity = f"Cemu {self.version_list[0]}"
         if new_activity != self.previous_activity and self.bot.ws:
             await self.bot.change_presence(status=discord.Status.online, activity=discord.Game(name=new_activity))
-            previous_activity = new_activity
+            self.previous_activity = new_activity
     
     @tasks.loop(minutes=2)
     async def updateVersions(self):
@@ -65,15 +67,19 @@ class Site(commands.Cog):
             print(err)
             traceback.print_exc()
     
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.channel.id == config.cfg["announcement_channel"]:
-            matches = re.search(r"Cemu (\d\.\d+\.\d+)", message.content)
-            if matches:
-                self.public_release_date = message.created_at + timedelta(days=7)
-                self.public_release_date.replace(tzinfo=timezone.utc)
-                self.patreon_release = matches.group(1)
-                await self.update_activity()
+    @tasks.loop(minutes=2)
+    async def updatePatreonStatus(self):
+        announcement_channel = self.bot.get_channel(config.cfg["announcement_channel"])
+        if announcement_channel != None:
+            async for message in announcement_channel.history(limit=5):
+                if message != None:
+                    matches = re.search(r"Cemu (\d\.\d+\.\d+)", message.content)
+                    if matches:
+                        self.public_release_date = message.created_at + timedelta(days=7)
+                        self.public_release_date.replace(tzinfo=timezone.utc)
+                        self.patreon_release = matches.group(1)
+                    break
+        await self.update_activity()
 
 def setup(bot):
     bot.add_cog(Site(bot))
