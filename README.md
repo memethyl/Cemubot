78d7296a6acfeb5853db301e94a2fc7984ef0770
Crementif <26669564+Crementif@users.noreply.github.com>
2021-07-20 01:03:17 +0200

    Add quote command, use slash commands and some fixes
    - The quote system adds a way to register, delete and list new slash commands or "quotes" that when triggered will give simple or complex responses.
    - The rest of the user-accessible commands have also been changed over to use the new slash commands.
    - The change to use aiohttp (suggested by the discord.py docs) might stop the Cemu version presence to prevent updating.
    - The rulesets change is to prevent situations where people have e.g. 7960MBs of RAM from triggering the less then sub 8GB warning since it's practically just that.

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ A custom bot for the [Cemu Discord server.](https://discord.gg/5psYsup)
 
 # Dependencies
 - Python 3.8 recommended (3.6.0 minimum)
+- [requests](https://docs.python-requests.org)
 - [discord.py](https://github.com/Rapptz/discord.py) 1.5.0+
+- [discord-py-slash-command](https://github.com/discord-py-slash-commands/discord-py-interactions)
 - [fuzzywuzzy](https://github.com/seatgeek/fuzzywuzzy)
 
 # Installation
 1. Download this repository
 2. Run setup.py and follow the instructions
-3. Run cemubot/cemubot.py
+3. (Optional) Make quote functionality operational by modifying the `guild_ids` and `base_permissions` variables in the `@cog_ext` sections of the quotes.py file
+4. Run cemubot/cemubot.py
---
+++ cemubot/cogs/quotes.py
@@ -0,0 +1,131 @@
+import discord
+from discord.ext import commands, tasks
+from discord_slash import cog_ext, SlashContext
+from discord_slash.client import SlashCommand
+from discord_slash.utils.manage_commands import get_all_commands, remove_slash_command, create_permission, create_option, create_choice
+
+import json
+import os
+from typing import Dict
+
+from cogs import config
+
+class Quote:
+    command = ""
+    base_command = ""
+    title = ""
+    content = ""
+    guild = 0
+    response_type = "embed"
+    slash_command = None
+
+    def __init__(self, slash : SlashCommand, command : str, base_command : str, guild : int, title : str, content : str, response_type : str):
+        self.command = command
+        self.base_command = base_command
+        self.guild = guild
+        self.title = title
+        self.content = content
+        self.response_type = response_type
+        if base_command:
+            self.slash_command = slash.add_subcommand(cmd=self.respond, base=base_command, name=self.command, guild_ids=[self.guild], base_default_permission=True)
+        else:
+            self.slash_command = slash.add_slash_command(cmd=self.respond, name=self.command, guild_ids=[self.guild], default_permission=True)
+
+    def to_embed(self):
+        if self.title:
+            return discord.Embed(colour=discord.Colour.from_rgb(0, 162, 221), title=self.title, description=self.content)
+        else:
+            return discord.Embed(colour=discord.Colour.from_rgb(0, 162, 221), description=self.content)
+    
+    def to_message(self):
+        if self.title:
+            return f"**{self.title}**: {self.content}"
+        else:
+            return self.content
+    
+    async def respond(self, ctx: SlashContext):
+        if self.response_type == "embed":
+            await ctx.send(embed=self.to_embed())
+        elif self.response_type == "text":
+            await ctx.send(content=self.to_message())
+    
+    def save(self):
+        return {"command": self.command, "base": self.base_command, "guild": self.guild, "title": self.title, "content": self.content, "response_type": self.response_type}
+
+    async def remove(self, bot, slash : SlashContext):
+        self.slash_command
+        del self.slash_command
+        # Manual requests are required to remove the synced commands
+        syncedCommands = await get_all_commands(bot.user.id, config.cfg["bot_token"], slash.guild.id)
+        for command in syncedCommands:
+            if command["name"] == self.command:
+                await remove_slash_command(bot.user.id, config.cfg["bot_token"], slash.guild.id, command["id"])
+            
+
+class Quotes(commands.Cog):
+    quotes : Dict[str, Quote] = {}
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if os.path.isfile("misc/quotes.json"):
+            self.load_quotes_from_file()
+        await self.bot.slash.sync_all_commands()
+    
+    def load_quotes_from_file(self):
+        self.quotes = {}
+        with open("misc/quotes.json", "r", encoding="utf-8") as f:
+            quotesJson = json.load(f)
+            for quoteJson in quotesJson:
+                self.quotes[quoteJson["command"]] = Quote(self.bot.slash, quoteJson["command"], quoteJson["base"], quoteJson["guild"], quoteJson["title"], quoteJson["content"], quoteJson["response_type"])
+    
+    def save_quotes_to_file(self):
+        with open("misc/quotes.json", "w") as f:
+            storeCommands = []
+            for quote in self.quotes.values():
+                storeCommands.append(quote.save())
+            json.dump(storeCommands, f, indent="\t")
+    
+    @cog_ext.cog_subcommand(guild_ids=[286429969104764928], base="quote", base_desc="Manages the quotes on this server.", base_default_permission=False, base_permissions={286429969104764928: [create_permission(291168448950566922, 1, True)]},
+    name="list", description="Lists all the existing quotes.")
+    async def quote_list(self, ctx: SlashContext):
+        listResponse = "Listing all commands:\n"
+        for quote in self.quotes.values():
+            listResponse += f" - {quote.command}"
+            if quote.base_command:
+                listResponse += f" (parent {quote.base_command})"
+            listResponse += "\n"
+        await ctx.send(content=listResponse)
+
+    @cog_ext.cog_subcommand(guild_ids=[286429969104764928], base="quote", base_desc="Manages the quotes on this server.", base_default_permission=False, base_permissions={286429969104764928: [create_permission(291168448950566922, 1, True)]},
+    name="add", description="Adds a new quote command.", options=[
+        create_option(name="name", description="Name of the new command that you want to add. Can't include any spaces!", option_type=3, required=True),
+        create_option(name="title", description="Title of the quote. Use \"None\" if you want to have no title.", option_type=3, required=True),
+        create_option(name="description", description="Description of the quote. Supports markdown!", option_type=3, required=True),
+        create_option(name="type", description="Type of the response", option_type=3, required=True, choices=[create_choice(name="Embed Response", value="embed"), create_choice(name="Text Response", value="text")]),
+        create_option(name="parent", description="Name of the parent command where this command name will be nested under.", option_type=3, required=False)
+    ])
+    async def quote_add(self, ctx: SlashContext, name : str, title: str, description : str, type : str, parent : str = ""):
+        if " " in name:
+            await ctx.send("You can't use spaces in the command names!")
+            return
+        if title.lower() == "none":
+            title = ""
+        self.quotes[name] = Quote(self.bot.slash, name.lower(), parent, ctx.guild.id, title, description, type)
+        await self.quotes[name].respond(ctx)
+        self.save_quotes_to_file()
+        await self.bot.slash.sync_all_commands()
+
+    @cog_ext.cog_subcommand(guild_ids=[286429969104764928], base="quote", base_desc="Manages the quotes on this server.", base_default_permission=False, base_permissions={286429969104764928: [create_permission(291168448950566922, 1, True)]},
+    name="delete", description="Deletes the given quote command.", options=[create_option(name="command", description="Command that's associated with the quote.", option_type=3, required=True)])
+    async def quote_delete(self, ctx: SlashContext, command : str):
+        await self.quotes[command].remove(self.bot, ctx)
+        del self.quotes[command]
+        await ctx.send(content="Successfully deleted the command!")
+        self.save_quotes_to_file()
+        await self.bot.slash.sync_all_commands(delete_from_unused_guilds=True, delete_perms_from_unused_guilds=True)
+    
+def setup(bot):
+    bot.add_cog(Quotes(bot))
